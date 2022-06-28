d09fe1a75022199725f2141848075cb6159db8a5
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-06-28 09:45:00 +0900

    Update css selector

--- packages/common-components/.storybook/preview.tsx
+++ packages/common-components/.storybook/preview.tsx
@@ -10,9 +10,7 @@ export const decorators: DecoratorFn[] = [
         styles={css`
           @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
 
-          *,
-          *::after,
-          *::before {
+          body {
             font-family: Pretendard, -apple-system, BlinkMacSystemFont,
               system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
               'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic',
