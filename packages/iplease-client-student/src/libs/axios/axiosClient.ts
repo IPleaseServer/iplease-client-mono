4aabf431b44d01e52736779c9cff0fa2a05697f6
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-08-07 20:27:28 +0900

    Add axiosClient

---
+++ packages/iplease-client-student/src/libs/axios/axiosClient.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosInstance as AxiosClient } from 'axios';
+
+const BASE_URL = 'https://d8a3-221-156-195-73.jp.ngrok.io';
+const URL_POSTFIX = '/api/v1';
+
+const axiosClient: AxiosClient = axios.create({
+  baseURL: `${BASE_URL + URL_POSTFIX}`,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE,PATCH,PUT,OPTIONS',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, X-Authentication-Token',
+  },
+  withCredentials: true,
+});
+
+export default axiosClient;
