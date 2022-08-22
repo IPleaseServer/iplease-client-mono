9fda1d7a9a30fe83fa88e0302fdd7601f22391dc
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-08-22 10:18:25 +0900

    Add requestHandler

---
+++ packages/iplease-client-student/src/utils/api/axios/requestHandler.ts
@@ -0,0 +1,47 @@
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
+import dayjs from 'dayjs';
+
+import { getValue, removeValue, setValue } from '@common/utils/storage/storage';
+
+import accountApiUri from '../uri/account';
+
+import axiosClient from '.';
+
+const refresh = (config: AxiosRequestConfig): AxiosRequestConfig => {
+  const refreshToken = getValue<string>('refreshToken');
+  const expireAt = getValue<string>('expiresAt');
+  let token = getValue<string>('accessToken');
+
+  // 토큰이 만료되었고, refreshToken 이 저장되어 있을 때
+  if (dayjs(expireAt).diff(dayjs()) < 0 && refreshToken) {
+    // 토큰 갱신 서버통신
+    axiosClient
+      .post(accountApiUri.refresh(), {
+        refreshToken,
+      })
+      .then(
+        (res: AxiosResponse<{ accessToken: string; refreshToken: string }>) => {
+          token = res.data.accessToken;
+          setValue<string>('accessToken', res.data.accessToken, true);
+          setValue<string>(
+            'expiresAt',
+            dayjs().add(1, 'hour').format('yyyy-MM-DD HH:mm:ss'),
+            true
+          );
+        }
+      );
+  }
+
+  if (config && config.headers && token) {
+    // eslint-disable-next-line no-param-reassign
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+};
+
+const refreshErrorHandle = (err: any) => {
+  removeValue('refreshToken', true);
+};
+
+export { refresh, refreshErrorHandle };
