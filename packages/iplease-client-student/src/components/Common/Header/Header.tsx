48a507e0537421cf12b3cacdf5ceca6cec0183f6
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-08-23 20:38:42 +0900

    Implement Header component

--- packages/iplease-client-student/src/components/Common/Header/Header.tsx
+++ packages/iplease-client-student/src/components/Common/Header/Header.tsx
@@ -1,14 +1,49 @@
 import { css } from '@emotion/react';
 
+import { colors, theme } from '@common/styles';
+
+import Logo from 'assets/Logo';
+
 const Header: React.FC = () => {
   const style = css`
     width: 100%;
-    height: 100vh;
+    height: 4.875rem;
     display: grid;
     place-items: center;
+    nav {
+      width: 70.5rem;
+      height: inherit;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      ul {
+        height: inherit;
+        display: flex;
+        align-items: center;
+        gap: 2.5rem;
+        justify-content: space-between;
+        li {
+          height: inherit;
+          display: grid;
+          place-items: center;
+          font-weight: ${theme.palette.fontWeight.bold};
+          color: ${colors.black};
+        }
+      }
+    }
   `;
 
-  return <header css={style} />;
+  return (
+    <header css={style}>
+      <nav>
+        <Logo size="small" />
+        <ul>
+          <li>IP 할당 신청</li>
+          <li>내 정보</li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
--- packages/iplease-client-student/src/pages/home.tsx
+++ packages/iplease-client-student/src/pages/home.tsx
@@ -1,20 +1,9 @@
-import { css } from '@emotion/react';
-
 import { Header } from 'components/Common/Header';
 
-const HomePage: React.FC = () => {
-  const style = css`
-    width: 100%;
-    height: 100vh;
-    display: grid;
-    place-items: center;
-  `;
-
-  return (
-    <div css={style}>
-      <Header />
-    </div>
-  );
-};
+const HomePage: React.FC = () => (
+  <main>
+    <Header />
+  </main>
+);
 
 export default HomePage;
