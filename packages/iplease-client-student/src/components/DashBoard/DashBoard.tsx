25343c8210b85af35d11b9bb6e07d7be67ea2e48
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-08-25 14:09:27 +0900

    Implement Dashboard component

--- packages/iplease-client-student/src/components/DashBoard/DashBoard.tsx
+++ packages/iplease-client-student/src/components/DashBoard/DashBoard.tsx
@@ -1,13 +1,40 @@
+import { css } from '@emotion/react';
+
 import { Heading } from '@common/components';
 
+import { AssignmentIpCancelDemandTable } from 'components/AssignmentIpCancelDemandTable';
+import { AssignmentIpCancelReserveTable } from 'components/AssignmentIpCancelReserveTable';
+import { AssignmentIpDemandTable } from 'components/AssignmentIpDemandTable';
 import { AssignmentIpTable } from 'components/AssignmentIpTable';
 
-const DashBoard = () => (
-  <div>
-    <Heading level="h1" text="IP 할당 신청" />
-    <Heading level="h3" text="할당 IP 목록" color="black" />
-    <AssignmentIpTable />
-  </div>
-);
+const DashBoard = () => {
+  const style = css`
+    #flex-wrapper {
+      display: flex;
+      gap: 2.5rem;
+      margin-bottom: 3.125rem;
+    }
+  `;
+
+  return (
+    <div css={style}>
+      <Heading level="h1" text="IP 할당 신청" />
+      <Heading level="h3" text="할당 IP 목록" color="black" />
+      <AssignmentIpTable />
+      <div id="flex-wrapper">
+        <span>
+          <Heading level="h3" text="IP 할당 신청 목록" color="black" />
+          <AssignmentIpDemandTable />
+        </span>
+        <span>
+          <Heading level="h3" text="IP 할당 신청 목록" color="black" />
+          <AssignmentIpCancelDemandTable />
+        </span>
+      </div>
+      <Heading level="h3" text="IP 할당 신청 목록" color="black" />
+      <AssignmentIpCancelReserveTable />
+    </div>
+  );
+};
 
 export default DashBoard;
