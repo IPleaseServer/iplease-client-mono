5e0a96babec46617c3c80f7e18625d3b1fbff9b4
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-08-25 14:19:11 +0900

    Add RightArrow svg to TableButton

--- packages/iplease-client-student/src/components/Common/TableButton/TableButton.tsx
+++ packages/iplease-client-student/src/components/Common/TableButton/TableButton.tsx
@@ -2,6 +2,8 @@ import { css } from '@emotion/react';
 
 import { colors, theme } from '@common/styles';
 
+import RightArrow from 'assets/RightArrow';
+
 const TableButton: React.FC = () => {
   const style = css`
     display: grid;
@@ -14,6 +16,10 @@ const TableButton: React.FC = () => {
       cursor: pointer;
       font-weight: ${theme.palette.fontWeight.regular};
       font-size: ${theme.palette.fontSize.extraSmall};
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      gap: 0.625rem;
     }
   `;
 
@@ -21,6 +27,7 @@ const TableButton: React.FC = () => {
     <div css={style}>
       <button type="button" onClick={() => console.log('a')}>
         IP 신청하기
+        <RightArrow />
       </button>
     </div>
   );
