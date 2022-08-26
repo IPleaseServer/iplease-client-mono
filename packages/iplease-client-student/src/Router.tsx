525d392c412fbdcd0d0bff3325587f162643da23
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-08-26 16:03:07 +0900

    Add resetPassword page

--- packages/iplease-client-student/src/Router.tsx
+++ packages/iplease-client-student/src/Router.tsx
@@ -1,14 +1,16 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
+import ResetPasswordPage from './pages/auth/reset-password';
 import SignInPage from './pages/auth/signin';
 import SignUpPage from './pages/auth/signup';
 import HomePage from './pages/home';
 
 const AppRouter: React.FC = () => (
   <Routes>
-    <Route path="/" element={<Navigate to="/home" replace />} />
+    <Route path="/" element={<SignInPage />} />
     <Route path="/auth/signup" element={<SignUpPage />} />
     <Route path="/auth/signin" element={<SignInPage />} />
+    <Route path="/auth/reset-password" element={<ResetPasswordPage />} />
     <Route path="/home" element={<HomePage />} />
   </Routes>
 );
