41a556110289f00868682b604a714e92131f9590
iseolin76 <iseolin76@gmail.com>
2022-08-27 18:29:32 +0900

    Create login button component

---
+++ packages/iplease-client-teacher/src/components/loginButton/style.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const ButtonWrapper = styled.div`
+  p {
+    margin: 16px 0 0;
+    padding: 0;
+    text-align: center;
+  }
+`;
+
+export default ButtonWrapper;
