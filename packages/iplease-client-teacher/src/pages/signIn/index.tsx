6cb8c0e5db67b433e55ddcbde110456a60912ab7
iseolin76 <iseolin76@gmail.com>
2022-08-27 18:31:37 +0900

    Update signIn page

--- packages/iplease-client-teacher/src/pages/signIn/index.tsx
+++ packages/iplease-client-teacher/src/pages/signIn/index.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import toast from 'react-hot-toast';
+
+import { Input } from '@common/components';
+
+import login from 'src/api/login';
+import Logo from 'src/asset/logo';
+import LoginButton from 'src/components/loginButton';
+
+import * as S from './style';
 
 function SignIn(): JSX.Element {
-  return <div />;
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  function onSubmit(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+    e.preventDefault();
+
+    if (!email || !password) {
+      toast.error('빈칸을 채워주세요.');
+      return;
+    }
+
+    login({ email, password });
+  }
+
+  return (
+    <S.Wrapper>
+      <Logo />
+      <S.Form>
+        <S.InputWrapper>
+          <Input
+            value={email}
+            onChange={e => {
+              const limitValue = e.currentTarget.value.slice(0, 6);
+              setEmail(limitValue);
+            }}
+            type="text"
+            placeholder="email"
+            postfix="@gsm.hs.kr"
+            max={6}
+          />
+          <Input
+            value={password}
+            onChange={e => setPassword(e.currentTarget.value)}
+            type="password"
+            placeholder="password"
+          />
+        </S.InputWrapper>
+        {/* eslint-disable-next-line react/jsx-no-bind */}
+        <LoginButton onClick={onSubmit} />
+      </S.Form>
+    </S.Wrapper>
+  );
 }
 
 export default SignIn;
---
+++ packages/iplease-client-teacher/src/pages/signIn/style.ts
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  gap: 24px;
+`;
+
+export const Form = styled.form`
+  width: 300px;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 24px;
+  gap: 7px;
+`;
