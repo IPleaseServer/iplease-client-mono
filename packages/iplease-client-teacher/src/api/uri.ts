a155e9f37264649daf278f19afa4796125b249e1
iseolin76 <iseolin76@gmail.com>
2022-08-27 18:12:01 +0900

    Create File with api uri set

---
+++ packages/iplease-client-teacher/src/api/uri.ts
@@ -0,0 +1,25 @@
+const BASE_URL = 'https://api.iplease.shop/api/v1/';
+
+export const changePassword = 'account/profile/command/password';
+
+export const register = 'account/register/teacher';
+
+export const login = 'account/login';
+
+export const refreshLogin = 'account/login/refresh';
+
+export const logout = 'account/logout';
+
+export function identifier(id: string) {
+  return `account/auth/teacher/${id}`;
+}
+
+export function authEmail(email: string) {
+  return `account/auth/email/${email}@gsm.hs.kr`;
+}
+
+export function authCode(code: string) {
+  return `account/auth/${code}`;
+}
+
+export default BASE_URL;
