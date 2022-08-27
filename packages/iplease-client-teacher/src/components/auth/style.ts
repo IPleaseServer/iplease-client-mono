3410eecd0247fcc119742971244a10a2c3758738
iseolin76 <iseolin76@gmail.com>
2022-08-27 18:21:47 +0900

    Create auth component

---
+++ packages/iplease-client-teacher/src/components/auth/style.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const AuthWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  button {
+    width: 60px;
+    margin-left: 6px;
+  }
+`;
+
+export default AuthWrapper;
