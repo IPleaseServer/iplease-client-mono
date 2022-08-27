ac8468b052851b3bd91987b6759f46f8c5cfa0d8
iseolin76 <iseolin76@gmail.com>
2022-08-27 17:50:59 +0900

    Update Add common-util

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -7,6 +7,9 @@ module.exports = {
   'packages/common-styles/**/*.+(ts|tsx)': [
     () => 'yarn tsc -p packages/common-styles/tsconfig.json --noEmit',
   ],
+  'packages/common-utils/**/*.+(ts|tsx)': [
+    () => 'yarn tsc -p packages/common-utils/tsconfig.json --noEmit',
+  ],
   'packages/iplease-client-teacher/**/*.+(ts|tsx)': [
     () => 'yarn tsc -p packages/iplease-client-teacher/tsconfig.json --noEmit',
   ],
