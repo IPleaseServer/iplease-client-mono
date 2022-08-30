4c25a50deda7137e2afeb552f500a2ce1fe42c8b
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-08-30 14:16:20 +0900

    Add td max-width

--- packages/iplease-client-student/src/components/AssignmentIpDemandTable/AssignmentIpDemandTable.tsx
+++ packages/iplease-client-student/src/components/AssignmentIpDemandTable/AssignmentIpDemandTable.tsx
@@ -21,6 +21,12 @@ const AssignmentIpDemandTable = () => {
         padding-bottom: 0.625rem;
       }
       tr {
+        .demand-title {
+          max-width: 10rem;
+        }
+        .demand-description {
+          max-width: 20rem;
+        }
         td {
           color: ${colors.black};
           padding: 0.3125rem 0;
@@ -41,8 +47,16 @@ const AssignmentIpDemandTable = () => {
         <th>설명</th>
         <th>상태</th>
         <tr>
-          <td>제목</td>
-          <td>아 진짜 짜증나네요</td>
+          <td className="demand-title">
+            안뇽하세요 저의 이름은 이선우라고 합니다 저이ㅡ 이름은 이선우라고
+            합니다 저ㅡ이 이름은 이선우라고 합니다
+          </td>
+          <td className="demand-description">
+            어쩔티비 저쩔티비 킹받쥬 쿠쿠루삥뽕 쩔티비 저쩔티비 킹받쥬
+            쿠쿠루삥뽕 쩔티비 저쩔티비 킹받쥬 쿠쿠루삥뽕 쩔티비 저쩔티비 킹받쥬
+            쿠쿠루삥뽕 쩔티비 저쩔티비 킹받쥬 쿠쿠루삥뽕 쩔티비 저쩔티비 킹받쥬
+            쿠쿠루삥뽕
+          </td>
           <td>검토 대기 중</td>
           <td>
             <Button text="신청 취소" size="small" color="negative" />
