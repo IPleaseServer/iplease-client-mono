69ee9fd4c2270b7e98d65337e26e16c82a5b4d9b
iseolin76 <iseolin76@gmail.com>
2022-08-31 17:14:02 +0900

    Create Base Table Component

---
+++ packages/iplease-client-teacher/src/components/table/base/style.ts
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled';
+
+export const TableWrapper = styled.article`
+  margin-top: 50px;
+`;
+
+export const TableName = styled.h2`
+  font-weight: ${({ theme }) => theme.palette.fontWeight.bold};
+  font-size: ${({ theme }) => theme.palette.fontSize.large};
+`;
+
+export const TableBox = styled.div`
+  display: flex;
+  padding: 20px 30px;
+  box-shadow: 0px 6px 12px rgba(51, 51, 51, 0.08);
+  border-radius: ${({ theme }) => theme.palette.borderRadius};
+  width: 40%;
+  margin-top: 15px;
+  table {
+    width: 100%;
+  }
+  th {
+    text-align: left;
+    font-weight: 700;
+    font-size: 15px;
+  }
+  thead,
+  tr {
+    width: 100%;
+    height: 35px;
+  }
+  td:nth-child(4) {
+    display: flex;
+    justify-content: flex-end;
+  }
+  div {
+    margin-top: 30px;
+    width: 200%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+  }
+`;
