58b74b6ad4f12f299f9da45b3cdda91124930414
iseolin76 <iseolin76@gmail.com>
2022-08-31 17:45:28 +0900

    Add Page url type

--- packages/iplease-client-teacher/src/config/url.ts
+++ packages/iplease-client-teacher/src/config/url.ts
@@ -1,4 +1,11 @@
-const URL = {
+interface URLType {
+  signUp: string;
+  signIn: string;
+  changePassword: string;
+  manageIpAssignments: string;
+}
+
+const URL: URLType = {
   signUp: '/signup',
   signIn: '/signin',
   changePassword: '/changePassword',
