d1083e91245c07d3a1c7f4c736b15f6eaf553a02
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-09-02 15:21:42 +0900

    Add assignIp interface

---
+++ packages/iplease-client-student/src/@types/internet-protocol.type.ts
@@ -0,0 +1,48 @@
+export interface IAssignIpInfo {
+  id: number;
+  ip: string;
+  assigneeId: number;
+  assignerId: number;
+}
+
+export interface IReleaseReserveAssignIpInfo {
+  id: number;
+  assignIpId: number;
+  issuerId: number;
+  releaseAt: number;
+}
+
+export interface IAssignIpResponse {
+  data: {
+    totalPages: number;
+    totalElements: number;
+    size: number;
+    content: IAssignIpInfo[];
+    number: number;
+    sort: {
+      empty: boolean;
+      unsorted: boolean;
+      sorted: boolean;
+    };
+    pageable: {
+      offset: number;
+      sort: {
+        empty: boolean;
+        unsorted: boolean;
+        sorted: boolean;
+      };
+      pageNumber: number;
+      pageSize: number;
+      paged: boolean;
+      unpaged: boolean;
+    };
+    numberOfElements: number;
+    first: boolean;
+    last: boolean;
+    empty: boolean;
+  };
+}
+
+export interface IReleaseReserveAssignIpResponse {
+  data: IReleaseReserveAssignIpInfo;
+}
