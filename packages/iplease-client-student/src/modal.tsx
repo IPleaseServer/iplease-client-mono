e798732b78785fff0bbae7d7443df6e3e3298806
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-09-05 17:03:58 +0900

    Add modal portal component

---
+++ packages/iplease-client-student/src/modal.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef, ReactNode } from 'react';
+
+import { createPortal } from 'react-dom';
+
+const modalRoot = document.querySelector('#modal') as HTMLElement;
+
+interface ModalProps {
+  children?: ReactNode;
+}
+
+const Modal = ({ children }: ModalProps) => {
+  const el = useRef(document.createElement('div'));
+
+  useEffect(() => {
+    const { current } = el;
+
+    modalRoot.appendChild(current);
+
+    // eslint-disable-next-line no-void
+    return () => void modalRoot.removeChild(current);
+  }, []);
+
+  return createPortal(children, el.current);
+};
+
+export default Modal;
