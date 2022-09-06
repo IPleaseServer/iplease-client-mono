8e5900da83c06288c604ef5dc67eec8e32e6307f
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-09-06 17:51:42 +0900

    Implement AssignIpReleaseDemand table

--- packages/iplease-client-student/src/components/AssignIpReleaseDemandTable/AssignIpReleaseDemandTable.tsx
+++ packages/iplease-client-student/src/components/AssignIpReleaseDemandTable/AssignIpReleaseDemandTable.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
+
 import { css } from '@emotion/react';
+import { AxiosResponse } from 'axios';
 
 import { Button } from '@common/components';
 import { colors, theme } from '@common/styles';
+import { getValue } from '@common/utils/storage/storage';
+
+import {
+  IAssignIpResponse,
+  IDemandReleaseAssignIpInfo,
+  IDemandReleaseAssignIpResponse,
+} from 'src/@types/internet-protocol.type';
+import axiosClient from 'utils/api/axios';
+import ipApiUri from 'utils/api/uri/internet-protocol';
+
+interface ITableData {
+  id: number;
+  ip: string;
+}
 
 const AssignIpReleaseDemandTable: React.FC = () => {
+  const [tableData, setTableData] = useState<ITableData[]>([]);
+
   const style = css`
     width: fit-content;
     padding: 1.25rem 1.875rem;
@@ -29,25 +48,77 @@ const AssignIpReleaseDemandTable: React.FC = () => {
         }
       }
     }
+    p {
+      font-size: ${theme.palette.fontSize.extraSmall};
+      color: ${colors.black};
+      font-weight: ${theme.palette.fontWeight.semiBold};
+    }
   `;
+
+  const handleTableData = () => {
+    const accountId = getValue<number>('accountId');
+
+    if (accountId) {
+      axiosClient.get(ipApiUri.queryDemandReleaseIp(0, accountId)).then(
+        ({
+          data: {
+            data: { content },
+          },
+        }: AxiosResponse<IDemandReleaseAssignIpResponse>) => {
+          if (content.length > 0) {
+            content.forEach(
+              (demandReleaseAssignIpInfo: IDemandReleaseAssignIpInfo) => {
+                axiosClient
+                  .get(
+                    ipApiUri.queryAssignIp(demandReleaseAssignIpInfo.assignIpId)
+                  )
+                  .then(
+                    ({
+                      data: {
+                        data: { ip },
+                      },
+                    }: AxiosResponse<IAssignIpResponse>) => {
+                      setTableData((prev: ITableData[]) => [
+                        ...prev,
+                        {
+                          id: demandReleaseAssignIpInfo.id,
+                          ip,
+                        },
+                      ]);
+                    }
+                  );
+              }
+            );
+          }
+        }
+      );
+    }
+  };
+
+  useEffect(() => {
+    handleTableData();
+  }, []);
 
   return (
     <div css={style}>
-      <table>
-        <th>IP 주소</th>
-        <tr>
-          <td>xxx.xxx.xxx.xxx</td>
-          <td>
-            <Button text="신청 취소" size="small" color="negative" />
-          </td>
-        </tr>
-        <tr>
-          <td>xxx.xxx.xxx.xxx</td>
-          <td>
-            <Button text="신청 취소" size="small" color="negative" />
-          </td>
-        </tr>
-      </table>
+      {tableData.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>IP 주소</th>
+            </tr>
+          </thead>
+          {tableData.map((demandReleaseIpInfo: ITableData) => (
+            <tr key={demandReleaseIpInfo.id}>
+              <td>{demandReleaseIpInfo.ip}</td>
+              <td>
+                <Button text="신청 취소" size="small" color="negative" />
+              </td>
+            </tr>
+          ))}
+        </table>
+      )}
+      {tableData.length === 0 && <p>할당 해제 신청된 IP가 존재하지 않아요</p>}
     </div>
   );
 };
