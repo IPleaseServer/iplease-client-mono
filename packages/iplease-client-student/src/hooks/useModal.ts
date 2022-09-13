aad7e157dce968653d1e619344786c5f3e7a0f0d
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-09-13 14:52:55 +0900

    Add useModal hook

---
+++ packages/iplease-client-student/src/hooks/useModal.ts
@@ -0,0 +1,26 @@
+import {
+  Dispatch,
+  RefObject,
+  SetStateAction,
+  useCallback,
+  useRef,
+} from 'react';
+
+const useModal = (
+  setModalVisible: Dispatch<SetStateAction<boolean>>
+): [RefObject<HTMLDivElement>, (e: MouseEvent) => void] => {
+  const el: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+
+  const clickOutside = useCallback(
+    (e: MouseEvent): void => {
+      if (el.current && !el.current.contains(e.target as Node)) {
+        setModalVisible(false);
+      }
+    },
+    [setModalVisible]
+  );
+
+  return [el, clickOutside];
+};
+
+export default useModal;
