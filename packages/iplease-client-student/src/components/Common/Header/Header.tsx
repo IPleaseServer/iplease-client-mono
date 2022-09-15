3d109e14da65780bf80932e68dd4237344fbbcf3
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-09-15 18:03:07 +0900

    Implement myinfo

--- packages/iplease-client-student/src/components/Common/Header/Header.tsx
+++ packages/iplease-client-student/src/components/Common/Header/Header.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
+
 import { css } from '@emotion/react';
 
+import { colors, theme } from '@common/styles';
+
 import Logo from 'assets/Logo';
+import MyInfoPopUp from 'components/MyInfoPopUp/MyInfoPopUp';
 import { NavItem } from 'components/NavItem';
 
 const Header: React.FC = () => {
+  const [wantMyInfo, setWantMyInfo] = useState<boolean>(false);
+
   const style = css`
     width: 100%;
     height: 4.875rem;
@@ -21,20 +28,33 @@ const Header: React.FC = () => {
         align-items: center;
         gap: 2.5rem;
         justify-content: space-between;
+        button {
+          font-size: ${theme.palette.fontSize.small};
+          font-weight: ${theme.palette.fontWeight.bold};
+          color: ${colors.black};
+          background: none;
+          border: none;
+          cursor: pointer;
+        }
       }
     }
   `;
 
   return (
-    <header css={style}>
-      <nav>
-        <Logo size="small" />
-        <ul>
-          <NavItem>IP 할당 신청</NavItem>
-          <NavItem>내 정보</NavItem>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header css={style}>
+        <nav>
+          <Logo size="small" />
+          <ul>
+            <NavItem>IP 할당 신청</NavItem>
+            <button type="button" onClick={() => setWantMyInfo(prev => !prev)}>
+              내 정보
+            </button>
+          </ul>
+        </nav>
+      </header>
+      {wantMyInfo && <MyInfoPopUp />}
+    </>
   );
 };
 
